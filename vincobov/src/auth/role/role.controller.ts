f56862c5fa56a3120380ebf4f2e166e6f1c83eba
marianagrisales <valesol294@gmail.com>
2026-04-08 15:26:36 -0500

    feat/Controles y Service de Role

--- vincobov/src/auth/role/role.controller.ts
+++ vincobov/src/auth/role/role.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+import { CreateRoleDto } from './dto/create-role.dto';
+import { UpdateRoleDto } from './dto/update-role.dto';
+import { RoleService } from './role.service';
 
 @Controller('roles')
-export class RoleController {}
+export class RoleController {
+  constructor(private readonly roleService: RoleService) {}
+
+  @Post()
+  create(@Body() createRoleDto: CreateRoleDto) {
+    return this.roleService.create(createRoleDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.roleService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.roleService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateRoleDto: UpdateRoleDto,
+  ) {
+    return this.roleService.update(id, updateRoleDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.roleService.remove(id);
+  }
+}
--- vincobov/src/auth/role/role.service.ts
+++ vincobov/src/auth/role/role.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+
+import { Role } from '../entities/role.entity';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+  private readonly roleRepository: Repository<Role>;
+
+  constructor(@InjectDataSource() private readonly dataSource: DataSource) {
+    this.roleRepository = this.dataSource.getRepository(Role);
+  }
+
+  async create(createRoleDto: CreateRoleDto): Promise<Role> {
+    const existingRole = await this.findByName(createRoleDto.name);
+    if (existingRole) {
+      throw new ConflictException(`El rol "${createRoleDto.name}" ya existe.`);
+    }
+
+    const newRole = this.roleRepository.create(createRoleDto);
+    return this.roleRepository.save(newRole);
+  }
+
+  async findAll(): Promise<Role[]> {
+    return this.roleRepository.find();
+  }
+
+  async findOne(id: number): Promise<Role> {
+    const role = await this.roleRepository.findOneBy({ id });
+    if (!role) {
+      throw new NotFoundException(`No se encontro el rol con id ${id}.`);
+    }
+    return role;
+  }
+
+  async update(id: number, updateRoleDto: UpdateRoleDto): Promise<Role> {
+    const role = await this.findOne(id);
+    const maybeName = (updateRoleDto as Partial<CreateRoleDto>).name;
+
+    if (maybeName && maybeName !== role.name) {
+      const roleWithSameName = await this.findByName(maybeName);
+      if (roleWithSameName && roleWithSameName.id !== id) {
+        throw new ConflictException(`El rol "${maybeName}" ya existe.`);
+      }
+    }
+
+    const updatedRole = this.roleRepository.merge(role, updateRoleDto);
+    return this.roleRepository.save(updatedRole);
+  }
+
+  async remove(id: number): Promise<{ message: string; id: number }> {
+    const role = await this.findOne(id);
+    await this.roleRepository.remove(role);
+
+    return {
+      message: 'Rol eliminado correctamente.',
+      id,
+    };
+  }
+
+  async findByName(name: string): Promise<Role | null> {
+    return this.roleRepository.findOneBy({ name });
+  }
+}
