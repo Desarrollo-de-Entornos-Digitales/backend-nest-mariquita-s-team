993e6546343008c485f529d63427834d6f2612b8
SamRWAZ <sam.burbano1@gmail.com>
2026-04-09 06:29:40 -0500

    fix/Error en compilación y creación de tablas

--- vincobov/src/auth/auth.module.ts
+++ vincobov/src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import type { StringValue } from 'ms';
 
 import { AuthController } from './auth.controller';
@@ -9,9 +10,14 @@ import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
+import { Permission } from './entities/permission.entity';
+import { RolePermission } from './entities/role-permission.entity';
+import { Role } from './entities/role.entity';
+import { User } from './entities/user.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([User, Role, Permission, RolePermission]),
     UserModule,
     RoleModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
